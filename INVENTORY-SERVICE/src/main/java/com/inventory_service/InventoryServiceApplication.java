9cf5828040b9f1e49b77ae067c2a10643413ed8d
manivannancsmca <manivannancsmca@gmail.com>
2026-07-07 02:43:10 +0530

    kafka ack mode configured

--- INVENTORY-SERVICE/src/main/java/com/inventory_service/InventoryServiceApplication.java
+++ INVENTORY-SERVICE/src/main/java/com/inventory_service/InventoryServiceApplication.java
@@ -2,8 +2,12 @@ package com.inventory_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- INVENTORY-SERVICE/src/main/java/com/inventory_service/entity/Inventory.java
+++ INVENTORY-SERVICE/src/main/java/com/inventory_service/entity/Inventory.java
@@ -1,6 +1,7 @@
 package com.inventory_service.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -9,15 +10,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(
-    name = "inventory",
-    uniqueConstraints = {
-        @UniqueConstraint(
-            name = "ik_inventory_productId",
-            columnNames = "productId"
-        )
-    }
-)
+@Table(name = "inventory", uniqueConstraints = {
+        @UniqueConstraint(name = "ik_inventory_productId", columnNames = "productId")
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -38,4 +33,19 @@ public class Inventory extends BaseEntity implements Serializable {
 
     @Column(nullable = false)
     private int reservedQuantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Inventory inventory = (Inventory) o;
+        return Objects.equals(id, inventory.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
